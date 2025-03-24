a47095b67e1f44a6fdcc3cdcf7451c6f5010255b
walkyy1707 <walky1707@gmail.com>
2025-03-24 17:47:46 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.c93c3cb5c5339803e241fe61f2152065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea783d57aaf68d6dba4b037c7691b98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993e1c0d9c8a92f9fde8",
+    "url": "/static/css/main.1fd788ca.chunk.css"
+  },
+  {
+    "revision": "d92ee8b856aeb4a0b7b8",
+    "url": "/static/js/2.2aac2638.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.2aac2638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993e1c0d9c8a92f9fde8",
+    "url": "/static/js/main.be7e442e.chunk.js"
+  },
+  {
+    "revision": "ad786a92536b3f3a4beb",
+    "url": "/static/js/runtime-main.9671abb4.js"
+  }
+]);
